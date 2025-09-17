44a1fecc49103727019cd06042dd6ed2d46feb75
Anggrahita <anggrahita_wardhani_33rpl@student.smktelkom-mlg.sch.id>
2025-09-17 08:49:27 +0700

    Update praktikum1.py

--- praktikum1.py
+++ praktikum1.py
@@ -6,15 +6,12 @@ str_numbers_to_int = int(str_numbers)
 print("Before casting :", str_numbers, ", the data type is", type(str_numbers))  
 print("After casting :", str_numbers_to_int, ", the data type is", type(str_numbers_to_int))  
 
-print(".....................................................")
-
 # casting from int to str  
 integer = 12345  
 integer_to_str = str(integer)  
 print("Before casting :", integer, ", the data type is", type(integer))  
 print("After casting :", integer_to_str, ", the data type is", type(integer_to_str))  
 
-print(".....................................................")
 
 # casting from int to bool  
 num_int = 1  
